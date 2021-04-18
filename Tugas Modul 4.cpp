7090aea1494e3aea9e4fe9b2287c55976d81aa2f
Rama Aryasuta Weihan <75559653+Ramaarya@users.noreply.github.com>
2021-04-18 15:26:19 +0700

    Update Tugas Modul 4.cpp

--- Tugas Modul 4.cpp
+++ Tugas Modul 4.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-	
+
+class Heading{
+public:
+	void methodHeading(){
+	cout << "======== E-Tiket Kereta Api ========= \n";
+	cout << "===================================== \n";
+	}
+};
+
+void pilihan_jalur(){
+	cout << "1. Jakarta - Semarang 	(Rp.50000/orang)\n";
+	cout << "2. Jakarta - Jogja 	(Rp.60000/orang)\n";
+	cout << "3. Jakarta - Surabaya 	(Rp.70000/orang)\n";
 }
