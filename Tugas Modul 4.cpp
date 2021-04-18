3ae88d78c7a69d0da893eaa313bd2384d4734dd8
Nanda Iqbal Hanafi <70440045+nandaiqbalh@users.noreply.github.com>
2021-04-18 15:29:49 +0700

    Menambahkan function aplikasi
    function ini berisi minta masukkan dari user, terus nantinya akan menampilkan tiket yang dipilih, jumlah, dan total biayanya.

--- Tugas Modul 4.cpp
+++ Tugas Modul 4.cpp
@@ -16,3 +16,31 @@ void pilihan_jalur(){
 	cout << "2. Jakarta - Jogja 	(Rp.60000/orang)\n";
 	cout << "3. Jakarta - Surabaya 	(Rp.70000/orang)\n";
 }
+void aplikasi(){
+	int a,b,hasil;
+	cout << "Masukan Pilihan Jalur [angka] : ";
+	cin >> a;
+	cout << "\n";
+	cout << "Jumlah tiket yang dipesan: ";
+	cin >> b;
+	
+	switch(a){
+			case 1:
+			cout << "Anda memilih Jakarta - Semarang \n";
+			hasil = b * 50000;
+			cout << "Total : Rp," << hasil << "\nJumlah tiket : " << b << " orang \n";
+			break;
+		case 2:
+			cout << "Anda memilih Jakarta - Jogja \n";
+			hasil = b * 60000;
+			cout << "Total : Rp," << hasil << "\nJumlah tiket : " << b << " orang \n";
+			break;
+		case 3:
+			cout << "Anda memilih Jakarta - Surabaya \n";
+			hasil = b * 70000;
+			cout << "Total : Rp," << hasil << "\nJumlah tiket : " << b << " orang \n";
+			break;
+		default:
+			cout << "Pilihan anda tidak tersedia";
+	}
+}
